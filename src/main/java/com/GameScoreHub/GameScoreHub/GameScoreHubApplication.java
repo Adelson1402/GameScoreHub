dfc799084287ab95ec66cf1d0fc489ab86b5409a
Adelson Santos <adelsonsantospinheiro@gmail.com>
2023-09-16 17:06:24 -0300

    primeiro cimmit

---
+++ src/main/java/com/GameScoreHub/GameScoreHub/GameScoreHubApplication.java
@@ -0,0 +1,13 @@
+package com.GameScoreHub.GameScoreHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameScoreHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameScoreHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/GameScoreHub/GameScoreHub/GameScoreHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GameScoreHub.GameScoreHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameScoreHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
